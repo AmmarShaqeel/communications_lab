fef3df006759d60d0583dd5a303b2b1935be46d9
ammarshaqeel <ammar903@gmail.com>
2017-12-01 15:25:02 +0000

    adding shit

--- script.m
+++ script.m
@@ -1,15 +1,45 @@
 global Tsampling fsampling
 Tsampling = 1e-7 ; fsampling = 1/Tsampling;
-alphabet = [-1, 1] ; Tb = 1e-6 ; Ts = Tb * log2(length(alphabet));
+unipolar_alphabet = [0,1];
+polar_alphabet = [-1,1];
+quaternary_alphabet = [-3,-1,1,3];
+
+Tb = 1e-6; 
 nbits = 1000; 
-nsymbols = nbits / log2(length(alphabet));
 N0 = 1;
-t = (0:Tsampling:nsymbols*Ts);
+
+load('tx_bits.mat');
+noise = sqrt(fsampling * N0 / 2) * randn(1,length(t));
+
+%unipolar binary
+Ts_unipolar = Tb * log2(length(unipolar_alphabet));
+unipolar_nsymbols = nbits / log2(length(unipolar_alphabet));
+unipolar_t = (0:Tsampling:unipolar_nsymbols*Ts_unipolar);
+unipolar_tx_symbols = map(tx_bits,unipolar_alphabet);
+unipolar_tx_signal = modulate(unipolar_tx_symbols,Ts_unipolar,unipolar_nsymbols) ;% Received signal (channel output)
+unipolar_rx_signal = unipolar_tx_signal + noise
+unipolar_rx_symbols = demodulate(unipolar_rx_signal,Ts_unipolar,unipolar_nsymbols);
+unipolar_rx_bits = demap(unipolar_rx_symbols,unipolar_alphabet);
+
+%polar binary
+Ts_polar = Tb * log2(length(polar_alphabet));
+polar_nsymbols = nbits / log2(length(polar_alphabet));
+polar_tx_symbols = map(tx_bits,polar_alphabet);
+polar_tx_signal = modulate(polar_tx_symbols,Ts_polar,polar_nsymbols) ;% Received signal (channel output)
+polar_rx_signal = polar_tx_signal + noise
+polar_rx_symbols = demodulate(polar_rx_signal,Ts_polar,polar_nsymbols);
+polar_rx_bits = demap(polar_rx_symbols,polar_alphabet);
+
+%quaternary
+Ts_quaternary = Tb * log2(length(quaternary_alphabet));
+quaternary_nsymbols = nbits / log2(length(quaternary_alphabet));
+quaternary_tx_symbols = map(tx_bits,quaternary_alphabet);
+quaternary_tx_signal = modulate(quaternary_tx_symbols,Ts_quaternary,quaternary_nsymbols) ;% Received signal (channel output)
+quaternary_rx_signal = quaternary_tx_signal + noise
+quaternary_rx_symbols = demodulate(quaternary_rx_signal,Ts_quaternary,quaternary_nsymbols);
+quaternary_rx_bits = demap(quaternary_rx_symbols,quaternary_alphabet);
 
 
-
-tx_symbols = map(tx_bits,alphabet);
-tx_signal = modulate(tx_symbols,Ts,nsymbols) ;% Received signal (channel output)
-rx_signal = tx_signal + sqrt(fsampling * N0 / 2) * randn(1,length(t));
-rx_symbols = demodulate(rx_signal,Ts,nsymbols);
-rx_bits = demap(rx_symbols,alphabet);
+%compare tx_bits and rx_bits
+error = (tx_bits == rx_bits);
+error = sum((error(:) == 0))
