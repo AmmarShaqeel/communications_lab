6a386aa70acfe3a6b4d715ed8f0930286f9a3af6
ammar903@gmail.com <ammar903@gmail.com>
2017-12-02 10:45:54 +0000

    some changes

--- script.m
+++ script.m
@@ -9,37 +9,45 @@ nbits = 1000;
 N0 = 1;
 
 load('tx_bits.mat');
-noise = sqrt(fsampling * N0 / 2) * randn(1,length(t));
 
 %unipolar binary
 Ts_unipolar = Tb * log2(length(unipolar_alphabet));
 unipolar_nsymbols = nbits / log2(length(unipolar_alphabet));
 unipolar_t = (0:Tsampling:unipolar_nsymbols*Ts_unipolar);
+unipolar_noise = sqrt(fsampling * N0 / 2) * randn(1,length(unipolar_t));
+
 unipolar_tx_symbols = map(tx_bits,unipolar_alphabet);
 unipolar_tx_signal = modulate(unipolar_tx_symbols,Ts_unipolar,unipolar_nsymbols) ;% Received signal (channel output)
-unipolar_rx_signal = unipolar_tx_signal + noise
+unipolar_rx_signal = unipolar_tx_signal + unipolar_noise;
 unipolar_rx_symbols = demodulate(unipolar_rx_signal,Ts_unipolar,unipolar_nsymbols);
 unipolar_rx_bits = demap(unipolar_rx_symbols,unipolar_alphabet);
 
 %polar binary
 Ts_polar = Tb * log2(length(polar_alphabet));
 polar_nsymbols = nbits / log2(length(polar_alphabet));
+polar_t = (0:Tsampling:polar_nsymbols*Ts_polar);
+polar_noise = sqrt(fsampling * N0 / 2) * randn(1,length(polar_t));
+
 polar_tx_symbols = map(tx_bits,polar_alphabet);
 polar_tx_signal = modulate(polar_tx_symbols,Ts_polar,polar_nsymbols) ;% Received signal (channel output)
-polar_rx_signal = polar_tx_signal + noise
+polar_rx_signal = polar_tx_signal + polar_noise;
 polar_rx_symbols = demodulate(polar_rx_signal,Ts_polar,polar_nsymbols);
 polar_rx_bits = demap(polar_rx_symbols,polar_alphabet);
 
 %quaternary
 Ts_quaternary = Tb * log2(length(quaternary_alphabet));
 quaternary_nsymbols = nbits / log2(length(quaternary_alphabet));
+quaternary_t = (0:Tsampling:quaternary_nsymbols*Ts_quaternary);
+quaternary_noise = sqrt(fsampling * N0 / 2) * randn(1,length(quaternary_t));
+
 quaternary_tx_symbols = map(tx_bits,quaternary_alphabet);
 quaternary_tx_signal = modulate(quaternary_tx_symbols,Ts_quaternary,quaternary_nsymbols) ;% Received signal (channel output)
-quaternary_rx_signal = quaternary_tx_signal + noise
+quaternary_rx_signal = quaternary_tx_signal + quaternary_noise;
 quaternary_rx_symbols = demodulate(quaternary_rx_signal,Ts_quaternary,quaternary_nsymbols);
 quaternary_rx_bits = demap(quaternary_rx_symbols,quaternary_alphabet);
 
 
 %compare tx_bits and rx_bits
-error = (tx_bits == rx_bits);
-error = sum((error(:) == 0))
+unipolar_error = length(tx_bits) - nnz(tx_bits == unipolar_rx_bits)
+polar_error = length(tx_bits) -  nnz(tx_bits == polar_rx_bits)
+quaternary_error = length(tx_bits) -  nnz(tx_bits == quaternary_rx_bits)
